619d5686bf9652077ce60d04e3d01da31baf97e8
mehradman <m.radmanmehr98@gmail.com>
2025-02-18 12:49:21 +0330

    Add Simle Price Calculator

---
+++ app.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	prices := []float64{10, 20, 30}
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+
+	results := make(map[float64][]float64)
+
+	for _, taxRate := range taxRates {
+		taxIncludedPrices := make([]float64, len(prices))
+		for priceIndex, price := range prices {
+			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
+		}
+		results[taxRate] = taxIncludedPrices
+	}
+
+	fmt.Println(results)
+}
